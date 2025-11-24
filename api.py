9ae3da8b36b2666828ce27e0e1dd6550c327872b
Irina Nugis <terigrander@yandex.ru>
2025-11-24 17:38:00 +0300

    task 24.7.2

--- api.py
+++ api.py
@@ -1,14 +1,17 @@
 import requests
 import json
 
+
 class PetFriends():
     '''API библиотека к веб приложению Pet Friends'''
     def __init__(self) -> None:
         self.base_url = 'https://petfriends.skillfactory.ru/'
 
-    def get_api_key(self, email: str, password: str) -> tuple[int, json]:
-        '''Метод делает запрос к API сервера и возвращает статус запроса и результат в формате
-        JSON с уникальным ключом пользователя, найденного по указанным email и паролю.'''
+    def get_api_key(self, email: str, password: str) -> json:
+        """Метод делает запрос к API сервера и возвращает статус
+        запроса и результат в формате JSON с уникальным ключом
+        пользователя, найденного по указанным email и паролю.
+        """
 
         endpoint_url = 'api/key'
         headers = {
@@ -22,58 +25,68 @@ class PetFriends():
         result = ''
         try:
             result = res.json()
-        except:
+        except Exception:
             result = res.text
         return status, result
-    
-    def get_pets_list(self, auth_key: json, filter: str = '') -> tuple[int, json]:
-        '''Метод делает запрос к API сервера и возвращает статус запроса и результат в формате JSON
-        со списком найденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
-        либо пустое значение - получить список всех питомцев, либо 'my_pets' - получить список собственных питомцев'''
+
+    def get_pets_list(self, auth_key: json, filter: str = '') -> json:
+        """Метод делает запрос к API сервера и возвращает статус
+        запроса и результат в формате JSON со списком найденных
+        питомцев, совпадающих с фильтром. На данный момент фильтр
+        может иметь либо пустое значение - получить список всех
+        питомцев, либо 'my_pets' - получить список собственных питомцев.
+        """
 
         endpoint_url = 'api/pets'
         header = {'auth_key': auth_key['key']}
         params = {'filter': filter}
 
-        res = requests.get(self.base_url + endpoint_url, headers=header, params=params)
+        res = requests.get(self.base_url + endpoint_url,
+                           headers=header,
+                           params=params)
 
         status = res.status_code
         result = ''
         try:
             result = res.json()
-        except:
+        except Exception:
             result = res.text
         return status, result
 
-# [POST] /api/pets (Добавление информации о новом питомце);
-# [DELETE] /api/pets/{pet_id} (Удаление питомца из базы данных);
-# [PUT] /api/pets/{pet_id} (Обновление информации о питомце).
-    def post_add_pet(self, auth_key: json, pet_name: str, pet_type: str, pet_age: int, pet_image: str):
-        '''Метод отправляет на сервер данные о добавляемом питомце и возвращает статус
-        запроса на сервер и результат в формате JSON с данными добавленного питомца'''
+    def post_add_pet(self, auth_key: json, pet_name: str,
+                     pet_type: str, pet_age: int, pet_image: str) -> json:
+        """Метод отправляет на сервер данные о добавляемом питомце и
+        возвращает статус запроса на сервер и результат в формате JSON
+        с данными добавленного питомца.
+        """
 
         endpoint_url = 'api/pets'
         header = {'auth_key': auth_key['key']}
         data = {
-                'name': pet_name, 
-                'animal_type': pet_type, 
+                'name': pet_name,
+                'animal_type': pet_type,
                 'age': pet_age
                 }
-        file = {'pet_photo': (pet_image, open(pet_image, 'rb'), 'image/jpeg')}
+        pet_photo = {'pet_photo': (pet_image, open(pet_image, 'rb'),
+                                   'image/jpeg')}
 
-        res = requests.post(self.base_url + endpoint_url, headers=header, data=data, files=file)
+        res = requests.post(self.base_url + endpoint_url,
+                            headers=header,
+                            data=data,
+                            files=pet_photo)
 
         status = res.status_code
         result = ''
         try:
             result = res.json()
-        except:
+        except Exception:
             result = res.text
         return status, result
 
-    def delete_pet(self, auth_key: json, pet_id: str):
-        '''Метод отправляет на сервер запрос на удаление питомца по указанному ID и возвращает
-        статус запроса'''
+    def delete_pet(self, auth_key: json, pet_id: str) -> json:
+        """Метод отправляет на сервер запрос на удаление питомца по
+        указанному ID и возвращает статус запроса.
+        """
 
         endpoint_url = 'api/pets' + '/' + pet_id
         header = {'auth_key': auth_key['key']}
@@ -81,22 +94,83 @@ class PetFriends():
         res = requests.delete(self.base_url + endpoint_url, headers=header)
 
         status = res.status_code
-        return status
+        result = ''
+        try:
+            result = res.json()
+        except Exception:
+            result = res.text
+        return status, result
 
-    def put_update_pet(self, auth_key: json, pet_id: str, pet_name: str, pet_type: str, pet_age: int):
-        '''Метод отправляет запрос на сервер об обновлении данных питомца по указанному ID и
-        возвращает статус запроса и result в формате JSON с обновлённыи данными питомца'''
+    def put_update_pet(self, auth_key: json, pet_id: str,
+                       pet_name: str, pet_type: str, pet_age: int) -> json:
+        """Метод отправляет запрос на сервер об обновлении данных
+        питомца по указанному ID и возвращает статус запроса и result
+        в формате JSON с обновлённыи данными питомца.
+        """
 
         endpoint_url = 'api/pets' + '/' + pet_id
         header = {'auth_key': auth_key['key']}
-        params = {'name': pet_name, 'age': pet_age, 'animal_type': pet_type }
+        params = {'name': pet_name, 'age': pet_age, 'animal_type': pet_type}
 
-        res = requests.put(self.base_url + endpoint_url, headers=header, data=params)
-        
+        res = requests.put(self.base_url + endpoint_url,
+                           headers=header,
+                           data=params)
+
         status = res.status_code
         result = ''
         try:
             result = res.json()
-        except:
+        except Exception:
+            result = res.text
+        return status, result
+
+    def post_add_pet_simple(self, auth_key: json, pet_name: str,
+                            pet_type: str, pet_age: int) -> json:
+        """Метод отправляет на сервер данные о добавляемом питомце без
+        фото и возвращает статус запроса на сервер и результат в
+        формате JSON с данными добавленного питомца.
+        """
+
+        endpoint_url = 'api/create_pet_simple'
+        header = {'auth_key': auth_key['key']}
+        data = {
+                'name': pet_name,
+                'animal_type': pet_type,
+                'age': pet_age
+                }
+
+        res = requests.post(self.base_url + endpoint_url,
+                            headers=header,
+                            data=data)
+
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except Exception:
+            result = res.text
+        return status, result
+
+    def post_add_pet_photo(self, auth_key: json,
+                           pet_id: str, pet_image: str) -> json:
+        """Метод отправляет запрос на сервер о добавлении фото питомца
+        по указанному id питомца и возвращает статус запроса на сервер
+        и результат в формате JSON с данными питомца, для которого
+        добавлено фото.
+        """
+
+        endpoint_url = 'api/pets/set_photo' + '/' + pet_id
+        header = {'auth_key': auth_key['key']}
+        file = {'pet_photo': (pet_image, open(pet_image, 'rb'), 'image/jpeg')}
+
+        res = requests.post(self.base_url + endpoint_url,
+                            headers=header,
+                            files=file)
+
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except Exception:
             result = res.text
         return status, result
--- tests/test_petfriends.py
+++ tests/test_petfriends.py
@@ -3,27 +3,73 @@ from settings import valid_email, valid_password
 
 pf = PetFriends()
 
-def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
-    '''Проверяем, что при передаче email и пароля существующего пользователя успешно 
-    возвращается ключ аутентификации.'''
 
-    status, result = pf.get_api_key(email, password) 
-    
+def get_first_of_my_pets_helper() -> tuple:
+    """Вспомогательная функция, возвращает код 1 и данные о первом
+    питомце в списке, полученном по фильтру my_pets, или код 0 и
+    сообщение что по этому фильтру нет питомцев.
+    """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_pets_list(auth_key, filter='my_pets')
+    if len(my_pets['pets']) > 0:
+        return 1, my_pets['pets'][0]
+    else:
+        return 0, 'There is no my pets'
+
+
+def test_get_api_key_for_valid_user(email=valid_email,
+                                    password=valid_password):
+    """Проверяем, что при передаче email и пароля существующего
+    пользователя успешно возвращается ключ аутентификации.
+    (Тест взят из модуля).
+    """
+
+    status, result = pf.get_api_key(email, password)
+
     assert status == 200
     assert 'key' in result
 
-def test_get_pets_list_with_valid_key(filter=''):
-    '''Проверяем, что успешно возвращается список питомцев.'''
 
-    _, auth_key = pf.get_api_key(valid_email, valid_password) 
+def test_get_api_key_invalid_credentials(email='11'+valid_email,
+                                         password='11'+valid_password):
+    """Проверяем, что при передаче несуществующих email и пароля ключ
+    аутентификации не возвращается.
+    """
+
+    status, result = pf.get_api_key(email, password)
+    assert status == 403
+    assert 'key' not in result
+
+
+def test_get_pets_list_with_valid_key(filter=''):
+    """Проверяем, что при передаче корректного ключа аутентификации
+    успешно возвращается список питомцев.
+    (Тест взят из модуля).
+    """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
     status, result = pf.get_pets_list(auth_key)
 
     assert status == 200
-    assert type(result) == dict
     assert len(result['pets']) != 0
-    
-def test_add_pet_with_valid_data(name='Alice', animal_type='tabby cat', age=6, image='images/cat1.jpg'):
-    '''Проверяем, что при запросе с корректными данными питомец успешно добавляется.'''
+
+
+def test_get_pets_list_with_invalid_key():
+    """Проверяем, что при передаче некорректного ключа аутентификации
+    возвращается код 403, а не список питомцев.
+    """
+
+    status, result = pf.get_pets_list({'key': '111'})
+    assert status == 403
+    assert 'Forbidden' in result
+
+
+def test_add_pet_with_valid_data(name='Alice', animal_type='tabby cat',
+                                 age=6, image='tests/images/cat1.jpg'):
+    """Проверяем, что при запросе с корректными данными питомец успешно
+    добавляется.
+    """
 
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     status, result = pf.post_add_pet(auth_key, name, animal_type, age, image)
@@ -33,27 +79,139 @@ def test_add_pet_with_valid_data(name='Alice', animal_type='tabby cat', age=6, i
     assert result['animal_type'] == animal_type
     assert result['age'] == str(age)
 
-def test_delete_existing_pet(pet_id='f2be51b3-950e-47d9-858b-47c451038370'):
-    '''Проверяем, что при передаче существующего pet_id питомец успешно удаляется.
-    NB! В данный момент сервер возвращает 200 даже при попытке удалить питомца по несуществующему id.'''
+
+def test_add_pet_negative_age(name='Alice', animal_type='cat',
+                              age=-1, image='tests/images/cat1.jpg'):
+    """Проверяем, что работает валидация возраста питомца - питомец
+    с отрицательным возрастом не добавляется.
+    """
 
     _, auth_key = pf.get_api_key(valid_email, valid_password)
-    status = pf.delete_pet(auth_key, pet_id)
+    status, _ = pf.post_add_pet(auth_key, name, animal_type, age, image)
+
+    assert status == 422
+
+
+def test_delete_existing_pet():
+    """Проверяем, что при передаче существующего pet_id питомец успешно
+    удаляется.
+    """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, test_pet = pf.post_add_pet_simple(auth_key, '111', '111', 11)
+
+    status, _ = pf.delete_pet(auth_key, test_pet['id'])
     _, check = pf.get_pets_list(auth_key, filter='my_pets')
     ids_list = list(map(lambda x: x['id'], check['pets']))
 
     assert status == 200
-    assert pet_id not in ids_list
+    assert test_pet['id'] not in ids_list
 
-def test_update_pet_with_valid_data(name='Lissa', animal_type='tabby cat', age=3):
-    '''Проверяем, что при передаче корректных данных информация о питомце успешно обновляются'''
+
+def test_delete_non_existing_pet():
+    """Проверяем, что при попытке удалить питомца по несуществующему id
+    возвращается ошибка.
+    """
 
     _, auth_key = pf.get_api_key(valid_email, valid_password)
-    _, pets = pf.get_pets_list(auth_key, filter='my_pets')
-    test_pet_id = pets['pets'][0]['id']
-    status, result = pf.put_update_pet(auth_key, test_pet_id, name, animal_type, age)
+    status, _ = pf.delete_pet(auth_key, '111111')
+
+    assert status == 403
+
+
+def test_update_pet_with_valid_data(name='Lissa',
+                                    animal_type='tabby cat',
+                                    age=3):
+    """Проверяем, что при передаче корректных данных информация о
+    питомце успешно обновляется.
+    """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    code, first_pet = get_first_of_my_pets_helper()
+
+    if code == 1:
+        test_pet_id = first_pet['id']
+        status, result = pf.put_update_pet(auth_key, test_pet_id, name,
+                                           animal_type, age)
+        assert status == 200
+        assert result['name'] == name
+        assert result['animal_type'] == animal_type
+        assert result['age'] == str(age)
+    else:
+        raise Exception("There is no my pets")
+
+
+def test_update_pet_with_blank_id(name='Lissa', animal_type='cat', age=2):
+    """Проверяем, что при передаче пустой строки в pet_id возвращается
+    код 404.
+    """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.put_update_pet(auth_key, '', name,
+                                       animal_type, age)
+
+    assert status == 200
+    assert 'name' not in result
+
+
+def test_add_pet_simple_valid_data(name='Lissa', animal_type='cat', age=2):
+    """Проверяем, что при запросе с корректными данными без фото
+    питомец успешно добавляется.
+    """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.post_add_pet_simple(auth_key, name, animal_type, age)
 
     assert status == 200
     assert result['name'] == name
     assert result['animal_type'] == animal_type
     assert result['age'] == str(age)
+
+
+def test_add_pet_simple_invalid_key(name='Lissa', animal_type='cat', age=2):
+    """Проверяем, что при запросе с некорректным ключом аутентификации
+    питомец не добавляется.
+    """
+
+    status, result = pf.post_add_pet_simple({'key': '111'}, name,
+                                            animal_type, age)
+
+    assert status == 403
+    assert 'name' not in result
+
+
+def test_add_pet_simple_negative_age(name='Lissa', animal_type='cat', age=-2):
+    """Проверяем, что работает валидация возраста питомца в запросе на
+    добавление питомца без фото - питомец с отрицательным возрастом не
+    добавляется.
+    """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.post_add_pet_simple(auth_key, name, animal_type, age)
+
+    assert status == 422
+    assert name not in result
+
+
+def test_add_pet_photo_for_existing_pet(image='tests/images/cat2.jpg'):
+    """Проверяем, фото добавляется к существующему питомцу без фото."""
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_pets_list(auth_key)
+    test_pet = list(filter(lambda x: x['pet_photo'] == '', my_pets['pets']))[0]
+
+    status, result = pf.post_add_pet_photo(auth_key, test_pet['id'], image)
+
+    assert status == 200
+    assert result['pet_photo'] != ''
+
+
+def test_add_pet_photo_for_non_existing_pet(image='tests/images/cat2.jpg'):
+    """Проверяем, фото добавляется к существующему питомцу без фото."""
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    status, result = pf.post_add_pet_photo(auth_key, '1111', image)
+
+    assert status == 404
+    assert 'pet_photo' not in result
